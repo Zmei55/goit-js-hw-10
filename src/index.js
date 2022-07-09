c103921d7761b3dc842e8b772d886ab16ab08fdc
Andrei <andrei.lerke@gmail.com>
2022-07-09 02:00:02 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import "./css/styles.css";
+import './css/styles.css';
+import countryInfoTpl from './templates/country-info.hbs';
+// import countryListTpl from './templates/country-list.hbs';
+// import Notiflix from 'notiflix';
 
-const DEBOUNCE_DELAY = 300;
+// const DEBOUNCE_DELAY = 300;
+
+const refs = {
+  countryInfo: document.querySelector('.country-info'),
+  countryList: document.querySelector('.country-list'),
+};
+
+// function fetchCountries(name) {}
+
+fetch('https://restcountries.com/v3.1/name/germany?fields=name,capital,population,languages,flags')
+  .then(r => r.json())
+  .then(countrys => {
+    console.log('countrys', countrys);
+    // countrys.map(c => renderCountryInfo(c));
+    countrys.map(({ languages }) => console.log(languages));
+  })
+  .catch(error => console.log(error));
+
+function renderCountryInfo(country) {
+  const markup = countryInfoTpl(country);
+  refs.countryInfo.innerHTML = markup;
+}
